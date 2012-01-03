963def388852dd037aeb34910b82b9a8779431b8
Geshan Manandhar <geshan+git@gmail.com>
2012-01-03 12:25:19 +0400

    Added bulb graphics to check schedule

--- application/views/schedule/check.php
+++ application/views/schedule/check.php
@@ -1,6 +1,8 @@
+<div class="ls-message-<?php print $msg_class;?>" id="ls-message-box">
 <h2 class="<?php print $msg_class;?>-light"><?php
 print $custom_message; ?></h2>
 
 
 <h3 class="<?php print $msg_class;?>-light"><?php
 print $full_message; ?></h3> 
+</div>
