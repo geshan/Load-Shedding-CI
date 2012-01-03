9a08cc18bd46aad1e961afa38f87dda2bb64cc41
Geshan Manandhar <geshan+git@gmail.com>
2012-01-03 10:53:33 +0400

    consmer working

--- application/controllers/consumer.php
+++ application/controllers/consumer.php
@@ -21,20 +21,20 @@ class Consumer extends CI_Controller {
   {
    
     
-    $url="http://http://localhost/wbs/index.php/schedule/check_current/4";
+    $url="http://localhost/wbs/index.php/schedule/check_current/4";
     $ch = curl_init();    // initialize curl handle
     curl_setopt($ch, CURLOPT_URL,$url); // set url to post to
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable
     curl_setopt($ch, CURLOPT_TIMEOUT, 4); // times out after 4s
     //curl_setopt($ch, CURLOPT_POSTFIELDS, $XPost); // add POST fields
     $result = curl_exec($ch); // run the whole proc
-    print $result;
-    print "working";
+    //print $result;
+    //print "working";
     //return $result;
-    /*$xml = new SimpleXMLElement($result);
-    $contents=(array)$xml;*/
+    $xml = new SimpleXMLElement($result);
+    $contents=(array)$xml;
     
-    //print_r($contents);
+    print_r($contents);
     
   }
   
