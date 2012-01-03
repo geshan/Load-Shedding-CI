7b310366d9d5f6fee625ddff4537049bb67b5bcc
Geshan Manandhar <geshan+git@gmail.com>
2012-01-03 11:27:41 +0400

    changed message of welcome controller

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,19 +1,12 @@
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to Web Services!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>It now has Load Sheddding Kathmandu live info.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-		
 		<?php
+		/*
 $dateTime = new DateTime("now", new DateTimeZone('Europe/Warsaw'));
 echo $dateTime->format("Y-m-d H:i:s");
 
@@ -21,7 +14,7 @@ $dateTimeZone = new DateTimeZone('Asia/Kathmandu');
 $dateTime->setTimezone($dateTimeZone);
 
 echo '<br/><br/>'. $dateTime->format("Y-m-d H:i:s");
-
+*/
 ?>
 	</div>
 
